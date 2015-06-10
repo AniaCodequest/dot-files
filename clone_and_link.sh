24ee38a50c4fc4584e2629e8dd6c6713f6c02e20
Anna Domańska-Grzyb <ania@codequest.com>
2015-06-10 14:33:17 +0200

    Revert to initial commit.

--- clone_and_link.sh
+++ clone_and_link.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-pushd "$HOME/Projects"
+pushd "$HOME"
 
-  if [ -d "dot-files" ]; then
-    pushd "dot-files"
+  if [ -d ".dot-files" ]; then
+    pushd ".dot-files"
       git smart-pull
     popd
   else
-    git clone "git://github.com/AniaCodequest/dot-files.git" "dot-files"
+    git clone "git://github.com/AniaCodequest/dot-files.git" ".dot-files"
   fi
 
-  ls -1d ~/Projects/dot-files/files/* ~/Projects/dot-files/files/.* | while read f; do
-    [ "$f" == '~/Projects/dot-files/files/.' ] ||
-    [ "$f" == '~/Projects/dot-files/files/..' ] ||
-    [ "$f" == '~/Projects/dot-files/files/.git' ] ||
-    ln -vsf "$f" ~
+  ls -1d .dot-files/files/* .dot-files/files/.* | while read f; do
+    [ "$f" == '.dot-files/files/.' ] ||
+    [ "$f" == '.dot-files/files/..' ] ||
+    [ "$f" == '.dot-files/files/.git' ] ||
+    ln -vsf "$f" .
   done
 
 popd
